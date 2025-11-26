a744118ce89c81dd9c9741adbc8a92784f4cba1f
AlphaxardGacuuru <alphaxardgacuuru47@gmail.com>
2025-11-26 23:18:32 +0300

    feat: updated cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // cacheName needs to be changed after any of the assets are changed
-const cacheName = 'ThePublicHomeCache8';
+const cacheName = 'ThePublicHomeCache9';
 
 const cacheAssets = [
 	'/js/app.js'
