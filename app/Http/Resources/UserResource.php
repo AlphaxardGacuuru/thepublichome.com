a75e4a3a5501803794ab27be8bfe4f119e4c9ca9
AlphaxardGacuuru <alphaxardgacuuru47@gmail.com>
2025-11-27 23:15:23 +0300

    fix: null

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -21,11 +21,11 @@ class UserResource extends JsonResource
             "phone" => $this->phone,
             "avatar" => $this->avatar,
             "accountType" => $this->account_type,
-            "membershipId" => $this->activeUserMembership()->membership?->id,
-            "membershipName" => $this->activeUserMembership()->membership?->name,
-            "membershipTier" => $this->activeUserMembership()->membership?->tier,
-            "membershipStatus" => $this->activeUserMembership()->status,
-            "membershipFeatures" => $this->activeUserMembership()->membership?->features,
+            "membershipId" => $this->activeUserMembership()?->membership->id,
+            "membershipName" => $this->activeUserMembership()?->membership->name,
+            "membershipTier" => $this->activeUserMembership()?->membership->tier,
+            "membershipStatus" => $this->activeUserMembership()?->status,
+            "membershipFeatures" => $this->activeUserMembership()?->membership->features,
             "createdAt" => $this->created_at,
         ];
     }
